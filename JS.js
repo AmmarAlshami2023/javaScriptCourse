49da4d16735c8b212fe85bfe626db1da56314808
momuhsen <128732674+momuhsen@users.noreply.github.com>
2023-03-24 16:11:48 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -5,14 +5,19 @@ let highscore = 0;
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
+
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+
+  
   if (!guess) 
   {
+
     document.querySelector('.message').textContent = ' No number!';
   }
    else if (guess === secretNumber) {
+
     document.querySelector('.message').textContent = 'Correct Number!';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
@@ -30,11 +35,14 @@ document.querySelector('.check').addEventListener('click', function () {
       score--;
       document.querySelector('.score').textContent = score;
     } else {
+
       document.querySelector('.message').textContent = 'You lost the game!'; 
       document.querySelector('.score').textContent = 0;
     }
   }
 })
+
+
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
